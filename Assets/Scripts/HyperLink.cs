eb891f818dd3a7cbfa49c7c98220b79021a82226
gilzoide <gilzoide@gmail.com>
2017-08-26 16:41:59 -0300

    External links to Yle website and API documentation

---
+++ Assets/Scripts/HyperLink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HyperLink : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	public Text tooltip;
+	public string url;
+
+	void Start() {
+		tooltip.text = url;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		tooltip.gameObject.SetActive(true);
+	}
+	public void OnPointerExit(PointerEventData eventData) {
+		tooltip.gameObject.SetActive(false);
+	}
+
+	public void OpenLink() {
+		Application.OpenURL(url);
+	}
+}
